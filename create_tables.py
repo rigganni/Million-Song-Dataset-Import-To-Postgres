ba19601ebb197ba99d5f77831a919efd33246d4b
Nick Riggan <riggan@pm.me>
2020-04-28 12:02:31 -0400

    feat: Set up to use local development environment

--- create_tables.py
+++ create_tables.py
@@ -1,24 +1,32 @@
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
+from os import environ
 
 
 def create_database():
+    # Get local development environment variables
+    if environ.get("UDACITY_DATA_ENGINEER_POSTGRES_INSTANCE"):
+        host = environ.get("UDACITY_DATA_ENGINEER_POSTGRES_INSTANCE")
+        user = environ.get("UDACITY_DATA_ENGINEER_POSTGRES_USER")
+        password = environ.get("UDACITY_DATA_ENGINEER_POSTGRES_PASSWORD")
+    dbname = "studentdb"
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn = psycopg2.connect("host={} dbname={} user={} password={}".format(host, dbname, user, password))
     conn.set_session(autocommit=True)
     cur = conn.cursor()
-    
+
     # create sparkify database with UTF8 encoding
     cur.execute("DROP DATABASE IF EXISTS sparkifydb")
     cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
 
     # close connection to default database
-    conn.close()    
-    
+    conn.close()
+
     # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    dbname = "sparkifydb"
+    conn = psycopg2.connect("host={} dbname={} user={} password={}".format(host, dbname, user, password))
     cur = conn.cursor()
-    
+
     return cur, conn
 
 
@@ -36,7 +44,7 @@ def create_tables(cur, conn):
 
 def main():
     cur, conn = create_database()
-    
+
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
